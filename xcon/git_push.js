4ab6d6e8b0a3affd676dfa0fe77816a5f1be8960
malikkurosaki <kurosakiblackangel@gmail.com>
2022-09-05 08:48:52 +0800

    update

--- xcon/git_push.js
+++ xcon/git_push.js
@@ -1,3 +1,4 @@
 const exec = require('child_process').execSync
 const path = require("path");
+
 exec(`git add . && git commit -m "update" && git push origin alfa/1`, { stdio: "inherit", cwd: path.resolve(__dirname, '../') });
