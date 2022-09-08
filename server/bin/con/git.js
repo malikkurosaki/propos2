ed5e42e42975e2019bcf2b16474cd82c83162019
malikkurosaki <kurosakiblackangel@gmail.com>
2022-09-08 09:59:56 +0800

    update && git push origin alpha/3

--- server/bin/con/git.js
+++ server/bin/con/git.js
@@ -6,8 +6,6 @@ const colors = require('colors');
 const exec = require('child_process').execSync;
 const _ = require('lodash');
 
-
-const ini = exec(`git branch`).toString().split("\n").find((e) => e.indexOf('*') === 0).toString().split(' ')[1].trim();
-exec(`git add . && git commit -m "update && git push origin ${ini}"`, { stdio: "inherit", cwd: path.resolve(__dirname, '../') });
-console.log("git push completed");
-
+const branch = exec(`git branch`).toString().split("\n").find((e) => e.indexOf('*') === 0).toString().split(' ')[1].trim();
+exec(`git add . && git commit -m "update && git push origin ${branch}"`, { stdio: "inherit", cwd: path.resolve(__dirname, '../') });
+console.log(`git push completed branch ${branch}`);
