e62a350cc5d55c2b21361976ab778170b41ba868
malikkurosaki <kurosakiblackangel@gmail.com>
2022-09-08 09:38:09 +0800

    update && git push origin alpha/3

--- server/bin/con/git.js
+++ server/bin/con/git.js
@@ -3,5 +3,11 @@
 const fs = require('fs');
 const path = require('path');
 const colors = require('colors');
-const execSync = require('child_process').execSync;
+const exec = require('child_process').execSync;
 const _ = require('lodash');
+
+
+const ini = exec(`git branch`).toString().split("\n").find((e) => e.indexOf('*') === 0).toString().split(' ')[1].trim();
+exec(`git add . && git commit -m "update && git push origin ${ini}"`);
+console.log("git push completed");
+
